aab244f1594956f0025f0a278d6402ca3d6edf17
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-04 15:14:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,10 +11,5 @@
   end
 
   def take_a_number(store, customer)
-    if store.count == 0
-      store.each_with_index {|customer, position| puts "Welcome, #{customer}. You are number #{position+1} in line."}
-    elsif store.count > 0
-      store.push(customer)
-      store.each_with_index {|customer, position| puts "Welcome, #{customer}. You are number #{position+1} in line."}
-    end
+
   end
